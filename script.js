44f379be3485f77340d0e99b2071cd3a3f2693ff
Aniketh Roy Chowdhury <95467658+anideva@users.noreply.github.com>
2025-11-07 23:45:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	function firstNonRepeatedChar(str) {
+  // Count each character
+  const count = {};
+
+  for (let char of str) {
+    count[char] = (count[char] || 0) + 1;
+  }
+
+  // Find first char with count 1
+  for (let char of str) {
+    if (count[char] === 1) {
+      return char;
+    }
+  }
+
+  return null; // No non-repeated character found
+}
+
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
